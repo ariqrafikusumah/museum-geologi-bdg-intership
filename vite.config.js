cc361d2288fa787cef8331a639706f33c3b72b94
ariqrafikusumah <ariqrafikusumah@gmail.com>
2023-01-23 06:44:20 +0700

    ChunkSize

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1600,
+    chunkSizeWarningLimit: 2600,
   },
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
